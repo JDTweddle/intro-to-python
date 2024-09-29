1fb303e0d661c68f24482d39ac98ad93e812f255
JDTweddle <jdtweddle@gmail.com>
2024-09-29 18:03:53 +0100

    038 complete

--- 038_dict_operations.py
+++ 038_dict_operations.py
@@ -67,7 +67,15 @@ print("Function: count_words_by_length")
 # Since there are two words of length 3, etc.
 
 def count_words_by_length(words):
-  pass
+  word_count = {}
+  for word in words:
+      word_length = len(word)
+      if word_length not in word_count:
+          word_count[word_length] = 1 
+      else:
+          word_count[word_length] += 1 
+  return word_count
+
 
 check_that_these_are_equal(
   count_words_by_length(["hat", "cat", "I", "bird"]),
@@ -80,3 +88,5 @@ check_that_these_are_equal(
 )
 
 # Once you're done, move on to 039_challenge_1_example.py
+
+# Done!
